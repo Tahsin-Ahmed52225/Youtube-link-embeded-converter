70b1565ab4f3662675131651b4efc5b4c6ab3f07
tahsin_dev <tahsinahmed52225@gmail.com>
2022-08-21 00:36:42 +0600

    This is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 $(window).on("load",function(){
-    //alert("HI")
+    $("#youtube_link").bind("paste", function(e){
+            let pastedData = e.originalEvent.clipboardData.getData('text');
+            let data = pastedData.replace('https://www.youtube.com/watch?v=','');
+            let newLink = `https://www.youtube.com/embed/`+data;
+            $('#embeded_link').attr('value',newLink)
+            $('iframe').attr('src',newLink);
+            //console.log(newLink);
+        } 
+    );
 })
